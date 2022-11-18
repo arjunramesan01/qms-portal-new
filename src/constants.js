f73061fa5261bae9fdf0e83dc29f16383240c581
arjunramesan01 <arjun.ramesan@byjus.com>
2022-11-18 15:46:48 +0530

    vanilla init

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-// Staging
-export const API_ENDPOINT = 'https://search-api-stg.byjusweb.com/byjus/web_search/'
-
-
-
-// Production
-// export const API_ENDPOINT = 'https://search-api.byjusweb.com/byjus/web_search/'
